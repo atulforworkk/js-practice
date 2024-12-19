00ccb2695a7bbfd11cdbace8876ba55d2a1c608e
Atul Kumar Tiwari <atulforworkk@gmail.com>
2024-12-19 18:27:20 +0530

    first commit

---
+++ js/FindTheNumerOfAlphabet.js
@@ -0,0 +1,19 @@
+let words = ['apple', 'banana', 'avocado', 'cherry'];
+
+const count = (arr,alphabet)=>{
+
+    let totalCount = 0;
+    for(let i =0; i<arr.length;i++){
+        let word = arr[i];
+                for (let j=0; j<word.length;j++){
+                            if(word[j]===alphabet){
+                                totalCount++
+                            }
+                }
+    }
+    return totalCount;
+}
+
+
+
+console.log( count(words,"p"));
