b866fa501816e36ccf70069c103e01ad90b2ccc9
Atul Kumar Tiwari <atulforworkk@gmail.com>
2024-12-24 01:54:54 +0530

    easy

---
+++ js/newFunction.js
@@ -0,0 +1,11 @@
+const number = [1,2,3,4,5,6,7,8,9,10];
+
+const mapArray = number.map(((element,index)=>{
+    sqaureRoot = Math.pow(element,2);
+        return sqaureRoot;
+}))
+
+console.log("🚀 ~ mapArray:", mapArray)
+
+let a = Math.sqrt(4);
+console.log("🚀 ~ a:", a)
---
+++ js/numberChallenge.js
@@ -0,0 +1,12 @@
+const tillHundred = Math.floor( Math.random()*100+1);
+const tillFifty = Math.floor( Math.random()*50+1);
+const sumOutput = tillFifty+tillHundred;
+console.log("🚀 ~ sumOutput:", sumOutput)
+const  differneceOutput = tillHundred-tillFifty;
+console.log("🚀 ~ differneceOutput:", differneceOutput)
+const productOutput = tillHundred*tillFifty;
+console.log("🚀 ~ productOutput:", productOutput)
+const quotientOutput = tillHundred/tillFifty;
+console.log("🚀 ~ quotientOutput:", quotientOutput)
+
+console.log("🚀 ~ tillHundred:", tillFifty);
